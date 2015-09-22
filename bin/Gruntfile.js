ff0fdaa662e578034e7a4399302ead189be41c72
micty <79230533@qq.com>
2015-09-22 22:52:10 +0800

    for house

--- bin/Gruntfile.js
+++ bin/Gruntfile.js
@@ -20,6 +20,7 @@ module.exports = function (grunt) {
 
 
     require('./tasks/default.js')(grunt);
+    require('./tasks/house.js')(grunt);
     require('./tasks/old.js')(grunt);
 
 
---
+++ src/partial/house/expose.js
@@ -0,0 +1,36 @@
+﻿
+
+//设置对外暴露的模块
+Module.expose({
+
+    //api
+    'API': true,
+    'Proxy': true,
+
+
+    //core
+    '$': true,
+    'MiniQuery': true,
+
+    //excore
+    'DOM': true,
+    'Edition': true,
+    'Mapper': true,
+    'Module': true,
+    'Style': true,
+    'Url': true,
+
+    //ui
+    'Dialog': true,
+    'Loading': true,
+    'Mask': true,
+    'Navigator': true,
+    'NoData': true,
+    'Panel': true,
+    'Tabs': true,
+    'Template': true,
+    'Toast': true,
+    'View': true,
+
+});
+
