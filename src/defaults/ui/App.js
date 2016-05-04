3a9bf9f14fa507904e0f34182e3769272be6a5df
micty <79230533@qq.com>
2016-05-04 15:08:46 +0800

    4.0.0

--- src/defaults/ui/App.js
+++ src/defaults/ui/App.js
@@ -12,7 +12,7 @@ define('App.defaults', /**@lends App.defaults*/ {
     animated: true,
 
     slide: {
-        width: 0.20,    //向右滑动的距离超过该值并松开滑动后才会触发滑动后退。
+        width: 0.25,    //向右滑动的距离超过该值并松开滑动后才会触发滑动后退。
         k: 0.6,         //斜率
         time: 300,      //过渡时间，单位ms
     },
